59df22d2e65105f77f06cdc9c349da72a4d2b941
Seth <seth@DavidszeMacBook.localdomain>
2023-04-13 07:35:55 -0400

    fixed farm

--- src/main/java/com/zipcodewilmington/froilansfarm/Farm.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Farm.java
@@ -3,14 +3,14 @@ package com.zipcodewilmington.froilansfarm;
 import java.util.ArrayList;
 
 public class Farm {
-    ArrayList<ChickenCoop> coop;
-    ArrayList<Stable> stable;
-    FarmHouse house;
-    ArrayList<Field> fields;
-    ArrayList<Vehicle> garage;
-    ArrayList<Corn> cornSilo;
-    ArrayList<Tomato> tomatoSilo;
-    ArrayList<Egg> carton;
+    ArrayList<ChickenCoop> coop = new ArrayList<>();
+    ArrayList<Stable> stable = new ArrayList<>();
+    FarmHouse house = new FarmHouse();
+    ArrayList<Field> fields = new ArrayList<>();
+    ArrayList<Vehicle> garage = new ArrayList<>();
+    ArrayList<Corn> cornSilo = new ArrayList<>();
+    ArrayList<Tomato> tomatoSilo = new ArrayList<>();
+    ArrayList<Egg> carton = new ArrayList<>();
 
 
  }
